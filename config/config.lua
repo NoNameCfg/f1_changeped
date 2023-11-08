1713ea036bc8960151624d56e8edcae69273d3bb
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2023-11-08 05:31:42 +0000

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.notify = "default" -- default or okok
 
-Config.framework = "old-esx" -- old-esx or new-esx or old-qb or new-qb
+Config.framework = "new-esx" -- old-esx or new-esx or old-qb or new-qb
 
 Config.webhook = "PUT_YOUR_WEBHOOK"
